2b715a16b324fae51c85b32a8e5943315dca92c0
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2024-08-08 12:39:43 +0800

    add projects

---
+++ src/projects.js
@@ -0,0 +1,34 @@
+export const projects = [
+    {
+        name: "https://hxfeng.netlify.app/",
+        url: "https://hxfeng.netlify.app/", 
+        subtitle: "This Website!",
+        description: "Used Astro with JavaScript, HTML, CSS to create this website. I'll be adding more content soon!",
+        year: 2024,
+        github: "https://github.com/hailenwashere/website"
+    },
+    {
+        name: "Our Fridge",
+        url: "https://docs.google.com/presentation/d/1S0mvZn8IOAFx1zNTJKhgl6p0fosIClARI3vkAeLVTZE/edit#slide=id.g93a1b1e5a5_0_328", 
+        subtitle: "An easy ingredient sharing website that fights food waste",
+        description: "A project created in Hack with a Pro 2024 that won 3rd place overall. Users can share leftover ingredients with their friends through a communal fridge which can be accessed through a unique code. Our Fridge uses email notifications to let a friend know that a user wants an ingredient, and generates easy recipes that can use the leftover ingredients accumulated by the group. I used React.js to implement ingredient requesting and fridge display (organized by food category). Shoutout to Yifan, Jacky, Warrick, Chanh, and Ellen for being a great team!",
+        year: 2024,
+        github: "https://github.com/hailenwashere/hackwapro"
+    },
+    {
+        name: "FocusView",
+        url: "https://docs.google.com/presentation/d/1DxxgkZjpErOBOf9MRxZmQdBL5_cyuvxsRUXvfZBGqf0/edit?usp=sharing", 
+        subtitle: "A web application that uses computer vision to combat bad habits while working/studying",
+        description: "A project created with UCLA Creative Labs in Spring 2024. To help students control their bad working habits such as bad posture, phone usage, and more, FocusView tracks user eye movement and posture data in each session with a pomodoro timer. I used Firebase to implement a smooth user authentication experience and React.js to create the frontend of the app, including the user profile, a session timer, and login pages.",
+        year: 2024,
+        github: "https://github.com/ArjunNair17/FocusView"
+    },
+    {
+        name: "Clearify",
+        url: "https://chromewebstore.google.com/detail/clearify/dfngmknjomojmdmnockpdpmpnbbhibfi", 
+        subtitle: "A skincare retail browser extension that scans the ingredients list of products as you shop",
+        description: "A project created with UCLA Creative Labs in Fall 2023/Winter 2024. Clearify is a cosmetic shopping browser extension that scans a product’s ingredients list as you shop and checks if a cosmetic is safe for your skin, e.g. silicone-free, alcohol-free, or FA-safe (fungal acne-safe), using the community-trusted EWG Skin Deep Database. I used Manifest V3, JavaScript, HTML, and CSS to add highlights and property counts to the extension that matched the sleek design and feel of Clearify.",
+        year: 2024,
+        github: "https://github.com/ArjunNair17/FocusView",
+    },
+]
